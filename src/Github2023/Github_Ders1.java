b5140b961e3139c690ab7f5f761fff206303e8e9
MehmetGül <mhmtchn.gl.06@gmail.com>
2023-03-28 22:06:11 +0300

    Initial commit

--- src/Github2023/Github_Ders1.java
+++ src/Github2023/Github_Ders1.java
@@ -3,5 +3,7 @@ package Github2023;
 public class Github_Ders1 {
     public static void main(String[] args) {
         System.out.println("bu sefer de olmazsa hiç olmasın");
+        System.out.println("bu sefer de olmazsa hiç olmasın");
+
     }
 }
